42053125cac92e24150cdbde081e2716c6014682
birunqi <runqi.bi@mail.utoronto.ca>
2018-11-21 20:05:18 -0500

    Add ColorBoardManager

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/ColorBoardManager.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/ColorBoardManager.java
@@ -1,22 +1,55 @@
 package fall2018.csc2017.slidingtiles;
 
-public class ColorBoardManager implements GameManageable {
-    private ColorBoard board;
-    private int score;
-    public ColorBoard getGame(){
-        return board;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ColorBoardManager {
+    ColorBoard colorBoard;
+    private int score = 0;
+    private int complexity;
+    boolean[][] board;
+
+    ColorBoardManager(int complexity) {
+        this.complexity = complexity;
+        this.colorBoard = new ColorBoard();
+        board = new boolean[8][10];
     }
 
-    @Override
-    public boolean puzzleSolved() {
-        return false;
-    }
-
-    @Override
     public int getScore() {
-        return 0;
+        return this.score;
     }
 
-    ;
+    public void changeColor(int newColor){
+        if(! puzzleSolved()){
+            changeOriColor(newColor);
+        }
+    }
 
+    public void changeOriColor(int newColor){
+        int initColor = colorBoard.getGrid(0, 0).getColor();
+        int x = 0;
+        int y = 0;
+        while(y < board[x].length && x < board.length && colorBoard.getGrid(x, y).getColor() == initColor){
+            colorBoard.getGrid(x, y).setColor(newColor);
+            while(x+1 < board.length && colorBoard.getGrid(x+1, y).getColor() == initColor) {
+                colorBoard.getGrid(x+1, y).setColor(newColor);
+                x++;
+            }
+            y++;
+            x = 0;
+        }
+        score++;
+    }
+
+    public boolean puzzleSolved() {
+        for (int x = 0; x < board.length; x++) {
+            for (int y = 0; y < board[x].length; y++) {
+                if (colorBoard.getGrid(x, y).getColor() != colorBoard.getGrid(0, 0).getColor()){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
