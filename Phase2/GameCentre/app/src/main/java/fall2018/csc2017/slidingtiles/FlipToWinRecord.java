b22716be3aa65a0578d7bdc019bfbc46daf27b33
quan <quan.xu@mail.utoronto.ca>
2018-11-22 02:44:07 -0500

    add recored and scoreboard for FlipToWin

---
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/FlipToWinRecord.java
@@ -0,0 +1,78 @@
+package fall2018.csc2017.slidingtiles;
+
+import android.annotation.SuppressLint;
+import android.support.annotation.NonNull;
+
+import java.io.Serializable;
+
+public class FlipToWinRecord implements Comparable<FlipToWinRecord>, Serializable {
+
+
+    /**
+     * The complexity of the game.
+     */
+    private int complexity = Main.INSTANCE.getFlipToWinBoardManager().getGame().getComplexity();
+
+
+    /**
+     * The current score of the game.
+     */
+    private int finalScore = Main.INSTANCE.getFlipToWinBoardManager().getScore();
+
+
+    /**
+     * The current username of the game.
+     */
+    private String userName = Main.INSTANCE.getUserManager().getCurrentUser();
+
+
+    /**
+     * Return a string type of record.
+     *
+     * @return a string type of record.
+     */
+    @SuppressLint("DefaultLocale")
+    String recordToString() {
+        String temp = "User: %s, took %d steps in game: %s, in complexity: %d * %d";
+        String gameName = Board.GAME_NAME;
+        return String.format(temp, userName, finalScore, gameName, complexity, complexity);
+    }
+
+
+    /**
+     * Return username
+     *
+     * @return username
+     */
+    String getUserName() {
+        return userName;
+    }
+
+    @Override
+    public int compareTo(@NonNull FlipToWinRecord anotherRecord) {
+        return (this.finalScore - anotherRecord.finalScore);
+    }
+
+
+    /**
+     * Return complexity
+     *
+     * @return complexity
+     */
+    int getComplexity() {
+        return this.complexity;
+    }
+
+
+    /**
+     * Return true if r has lower final score
+     *
+     * @param r is a record of the game.
+     * @return true if r has lower final score
+     */
+    boolean checkLowerScore(FlipToWinRecord r) {
+        return this.finalScore >= r.finalScore;
+    }
+
+
+}
