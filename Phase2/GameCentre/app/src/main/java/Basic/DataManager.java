093ae2fc49c853d72fc20e195c2913f2b87beada
Arieski <zhangzijinning@hotmail.com>
2018-11-24 00:52:07 -0500

    redesign DataManager, delete unnecessary attributes

--- Phase2/GameCentre/app/src/main/java/Basic/DataManager.java
+++ Phase2/GameCentre/app/src/main/java/Basic/DataManager.java
@@ -1,6 +1,5 @@
 package Basic;
 
-import Basic.UserManager;
 import ColorMatching.ColorBoardManager;
 import FlipToWin.FlipToWinBoardManager;
 import fall2018.csc2017.slidingtiles.BoardManager;
@@ -12,105 +11,57 @@ public enum DataManager {
     /**
      * The String that represent the current game name.
      */
-    private String currentGame;
+    private String currentGameName;
 
     /**
-     * the FlipToWinBoardManager of this game
+     * The String that represent the current user name.
      */
-    private BoardManager slideTileBoardManager;
-
-
-    /**
-     * The UserManager of this game
-     */
-    private UserManager userManager;
-
-    /**
-     * The ColorBoardManager of this game
-     */
-    private ColorBoardManager colorBoardManager;
-
-
-    /**
-     * The ScoreBoard of this game
-     */
-    private ScoreBoard scoreBoard;
-
-    /**
-     * The FlipToWinBoardManager of this game
-     */
-    private FlipToWinBoardManager flipToWinBoardManager;
+    private String currentUserName;
 
     /**
      * Return the current game name
      *
      * @return the current game name
      */
-    public String getCurrentGame() {
-        return currentGame;
+    public String getCurrentGameName() {
+        return currentGameName;
     }
 
     /**
-     * Set the currentGame to the given game name
+     * Set the currentGameName to the given game name
      *
-     * @param currentGame the given current game name
+     * @param currentGameName the given current game name
      */
-    public void setCurrentGame(String currentGame) {
-        this.currentGame = currentGame;
+    public void setCurrentGameName(String currentGameName) {
+        this.currentGameName = currentGameName;
     }
 
     /**
-     * Return the FlipToWinBoardManager
-     */
-    FlipToWinBoardManager getFlipToWinBoardManager() {
-        return this.flipToWinBoardManager;
-    }
-
-
-    /**
-     * Return the ColorBoardManager
-     */
-    ColorBoardManager getColorBoardManager(){ return this.colorBoardManager;}
-
-    /**
-     * Set the FlipToWinBoardManager for loading function.
+     * Return the name of the current user
      *
-     * @param fbm the FlipToWinBoardManager instance will be loaded
+     * @return current user name
      */
-    void setFlipToWinBoardManager(FlipToWinBoardManager fbm) {
-        this.flipToWinBoardManager = fbm;
+    public String getCurrentUserName() {
+        return currentUserName;
     }
 
     /**
-     * Set the ColorBoardManager for loading function.
+     * Set currentUserName to the given game name
      *
-     * @param cbm the ColorBoardManager instance will be loaded
+     * @param currentUserName the given game name
      */
-    void setColorBoardManager(ColorBoardManager cbm) {
-        this.colorBoardManager = cbm;
-    }
-
-    /**
-     * This will create a new instance of FlipToWinBoardManager for new game function
-     */
-    void startNewFlipToWinGame(int complexity) {
-        this.flipToWinBoardManager = new FlipToWinBoardManager(complexity);
-    }
-
-    /**
-     * This will create a new instance of FlipToWinBoardManager for new game function
-     */
-    void startNewColorMatchingGame(int complexity) {
-        this.colorBoardManager = new ColorBoardManager(complexity);
+    public void setCurrentUserName(String currentUserName) {
+        this.currentUserName = currentUserName;
     }
 
 
-    /**
-     * Return the boardManager
-     */
-    public BoardManager getBoardManager() {
-        return slideTileBoardManager;
-//        switch (currentGame){
+
+//    /**
+//     * Return the boardManager
+//     */
+//    public BoardManager getBoardManager() {
+//        return slideTileBoardManager;
+//        switch (currentGameName){
 //            case "ST":
 //                return slideTileBoardManager;
 //            case "Color":
@@ -118,88 +69,35 @@ public enum DataManager {
 //            case "FTW":
 //                return flipToWinBoardManager;
 //        }
-    }
+//    }
+//
+//
+//    /**
+//     * Set the boardManager for loading function.
+//     *
+//     * @param object the FlipToWinBoardManager instance will be loaded
+//     */
+//    public void setBoardManager(Object object) {
+//        switch (currentGameName){
+//            case "ST":
+//                this.slideTileBoardManager = (BoardManager) object;
+//                break;
+//            case "Color":
+//                this.colorBoardManager = (ColorBoardManager) object;
+//                break;
+//            case "FTW":
+//                this.flipToWinBoardManager = (FlipToWinBoardManager) object;
+//                break;
+//        }
+//    }
 
 
-    /**
-     * Set the boardManager for loading function.
-     *
-     * @param object the FlipToWinBoardManager instance will be loaded
-     */
-    public void setBoardManager(Object object) {
-        switch (currentGame){
-            case "ST":
-                this.slideTileBoardManager = (BoardManager) object;
-                break;
-            case "Color":
-                this.colorBoardManager = (ColorBoardManager) object;
-                break;
-            case "FTW":
-                this.flipToWinBoardManager = (FlipToWinBoardManager) object;
-                break;
-        }
-    }
+//    /**
+//     * This will create a new instance of FlipToWinBoardManager for new game function
+//     */
+//    void startNewGame(int complexity) {
+//        this.slideTileBoardManager = new BoardManager(complexity);
+//    }
 
 
-    /**
-     * This will create a new instance of FlipToWinBoardManager for new game function
-     */
-    void startNewGame(int complexity) {
-        this.slideTileBoardManager = new BoardManager(complexity);
-    }
-
-
-
-    /**
-     * Return the UserManager of the game
-     *
-     * @return the UserManager of the game
-     */
-    public UserManager getUserManager() {
-        return userManager;
-    }
-
-
-    /**
-     * Set the UserManager for loading function
-     *
-     * @param um the UserManager from the file
-     */
-    public void setUserManager(UserManager um) {
-        this.userManager = um;
-    }
-
-
-    /**
-     * Return the ScoreBoard
-     *
-     * @return the ScoreBoard of the game
-     */
-    public ScoreBoard getScoreBoard() {
-        return scoreBoard;
-    }
-
-
-    /**
-     * This will create a new instance of ScoreBoard for new game function
-     */
-    public void setScoreBoard(ScoreBoard sb) {
-        this.scoreBoard = sb;
-    }
-
-
-    /**
-     * Create an new UserManager
-     */
-    public void newUserManager() {
-        this.userManager = new UserManager();
-    }
-
-
-    /**
-     * Create an new ScoreBoard
-     */
-    public void newScoreBoard() {
-        this.scoreBoard = new ScoreBoard();
-    }
 }
