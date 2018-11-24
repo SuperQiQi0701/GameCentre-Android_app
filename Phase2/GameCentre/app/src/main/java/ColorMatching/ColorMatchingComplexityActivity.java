a31d04b38f0798bf9e96e91fb8fa5c2eff9ecac5
Shaun Liu <shaun.liu@mail.utoronto.ca>
2018-11-24 11:43:25 -0500

    Add ColorMatchingComplexityActivity

---
+++ Phase2/GameCentre/app/src/main/java/ColorMatching/ColorMatchingComplexityActivity.java
@@ -0,0 +1,88 @@
+package ColorMatching;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import Basic.Main;
+
+import fall2018.csc2017.slidingtiles.R;
+
+public class ColorMatchingComplexityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_color_matching_complexity);
+        setupComplexity1ButtonListener();
+        setupComplexity2ButtonListener();
+        setupComplexity3ButtonListener();
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        TextView tv = findViewById(R.id.complexityTitle);
+        tv.setText("Choose A Level!");
+        tv.setTextColor(Color.BLACK);
+    }
+
+
+    /**
+     * Activate the Level 1 button.
+     */
+    private void setupComplexity1ButtonListener() {
+        Button complexity1Button = findViewById(R.id.color_complexity1_button);
+        complexity1Button.setBackgroundColor(Color.GREEN);
+        complexity1Button.setTextColor(Color.BLACK);
+        complexity1Button.setOnClickListener((v) -> setUpGame(3));
+
+    }
+
+
+    /**
+     * Activate the Level 2 button.
+     */
+    private void setupComplexity2ButtonListener() {
+        Button complexity2Button = findViewById(R.id.color_complexity2_button);
+        complexity2Button.setBackgroundColor(Color.YELLOW);
+        complexity2Button.setTextColor(Color.BLACK);
+        complexity2Button.setOnClickListener((v) -> setUpGame(4));
+    }
+
+
+    /**
+     * Activate the Level 1 button.
+     */
+    private void setupComplexity3ButtonListener() {
+        Button complexity3Button = findViewById(R.id.color_complexity3_button);
+        complexity3Button.setBackgroundColor(Color.RED);
+        complexity3Button.setTextColor(Color.BLACK);
+        complexity3Button.setOnClickListener((v) -> setUpGame(5));
+    }
+
+
+/*    @Override
+    public void onBackPressed() {
+        finish();
+        Intent temp = new Intent(this, StartingActivity.class);
+        startActivity(temp);
+    }*/
+
+    private void setUpGame(int complexity) {
+        Main.INSTANCE.startNewColorMatchingGame(complexity);
+        String fileName = "Auto_" + Main.INSTANCE.getUserManager().getCurrentUser() + ".ser";
+        Main.INSTANCE.saveBoardManagerToFile(this.getApplicationContext(), fileName);
+        Intent temp = new Intent(this, ColorMatchingActivity.class);
+        startActivity(temp);
+        finish();
+    }
+
+}
+
