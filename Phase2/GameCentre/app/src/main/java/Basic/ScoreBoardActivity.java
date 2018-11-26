e0cd5969c172425a9045e84902a0ac842f287340
Arieski <zhangzijinning@hotmail.com>
2018-11-25 23:20:01 -0500

    fixed scoreBoard display problem

--- Phase2/GameCentre/app/src/main/java/Basic/ScoreBoardActivity.java
+++ Phase2/GameCentre/app/src/main/java/Basic/ScoreBoardActivity.java
@@ -20,7 +20,7 @@ public class ScoreBoardActivity extends AppCompatActivity {
         ScoreBoard scoreBoard = FileManager.loadScoreBoard(this.getApplicationContext());
 
         //modify the TextView of the myScore
-        TextView myTextView = findViewById(R.id.ftw_myScore);
+        TextView myTextView = findViewById(R.id.myScore);
 
 
 //        String myScore = Integer.toString(INSTANCE.getFlipToWinBoardManager().getScore());
@@ -63,50 +63,50 @@ public class ScoreBoardActivity extends AppCompatActivity {
 
         //set the TextView for the first five record.
         ArrayList topFive = scoreBoard.TopFiveToString();
-        String noScore = "You don't have a current score because you have not won the current game " +
-                "yet.";
-        TextView no1TextView = findViewById(R.id.ftw_no1Record);
+//        String noScore = "You don't have a current score because you have not won the current game " +
+//                "yet.";
+        TextView no1TextView = findViewById(R.id.no1Record);
         no1TextView.setText((String) topFive.get(0));
 
-        TextView no2TextView = findViewById(R.id.ftw_no2Record);
+        TextView no2TextView = findViewById(R.id.no2Record);
         no2TextView.setText((String) topFive.get(1));
 
-        TextView no3TextView = findViewById(R.id.ftw_no3Record);
+        TextView no3TextView = findViewById(R.id.no3Record);
         no3TextView.setText((String) topFive.get(2));
 
-        TextView no4TextView = findViewById(R.id.ftw_no4Record);
+        TextView no4TextView = findViewById(R.id.no4Record);
         no4TextView.setText((String) topFive.get(3));
 
-        TextView no5TextView = findViewById(R.id.ftw_no5Record);
+        TextView no5TextView = findViewById(R.id.no5Record);
         no5TextView.setText((String) topFive.get(4));
 
         //modify the TextView of each text box
-        TextView titleTextView = findViewById(R.id.ftw_ScoreBoardTitle);
-        String title = "FlipToWinScoreboard";
+        TextView titleTextView = findViewById(R.id.ScoreBoardTitle);
+        String title = "Scoreboard";
         titleTextView.setText(title);
         titleTextView.setTextColor(Color.BLACK);
 
-        TextView TextView01 = findViewById(R.id.ftw_no1);
+        TextView TextView01 = findViewById(R.id.no1);
         String the1 = "No.1";
         TextView01.setText(the1);
         TextView01.setTextColor(Color.RED);
 
-        TextView TextView02 = findViewById(R.id.ftw_no2);
+        TextView TextView02 = findViewById(R.id.no2);
         String the2 = "No.2";
         TextView02.setText(the2);
         TextView02.setTextColor(Color.GREEN);
 
-        TextView TextView03 = findViewById(R.id.ftw_no3);
+        TextView TextView03 = findViewById(R.id.no3);
         String the3 = "No.3";
         TextView03.setText(the3);
         TextView03.setTextColor(Color.CYAN);
 
-        TextView TextView04 = findViewById(R.id.ftw_no4);
+        TextView TextView04 = findViewById(R.id.no4);
         String the4 = "No.4";
         TextView04.setText(the4);
         TextView04.setTextColor(Color.BLUE);
 
-        TextView TextView05 = findViewById(R.id.ftw_no5);
+        TextView TextView05 = findViewById(R.id.no5);
         String the5 = "No.5";
         TextView05.setText(the5);
         TextView05.setTextColor(Color.MAGENTA);
