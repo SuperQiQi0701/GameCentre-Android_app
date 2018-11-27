6e2acab2022c9372000b516b617b65aac471b74c
quan <quan.xu@mail.utoronto.ca>
2018-11-27 17:30:57 -0500

    set up unit-test

---
+++ Phase2/GameCentre/app/src/test/java/FlipToWin/FlipToWinBoardTest.java
@@ -0,0 +1,49 @@
+package FlipToWin;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FlipToWinBoardTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void numGrids() {
+    }
+
+    @Test
+    public void getColNum() {
+    }
+
+    @Test
+    public void getRowNum() {
+    }
+
+    @Test
+    public void getGrid() {
+    }
+
+    @Test
+    public void setGrid() {
+    }
+
+    @Test
+    public void makeMove() {
+    }
+
+    @Test
+    public String toString() {
+        return null;
+    }
+
+    @Test
+    public void iterator() {
+    }
+}
