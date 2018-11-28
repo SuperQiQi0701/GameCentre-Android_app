c49a89e726ec7d8608150fa6cbb4e5762ddda810
Arieski <zhangzijinning@hotmail.com>
2018-11-27 22:31:18 -0500

    create JUnit test for DataManager and FileManager Class

---
+++ Phase2/GameCentre/app/src/test/java/Basic/FileManagerTest.java
@@ -0,0 +1,38 @@
+package Basic;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FileManagerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void saveToFile() {
+    }
+
+    @Test
+    public void loadUserManager() {
+    }
+
+    @Test
+    public void loadScoreBoard() {
+    }
+
+    @Test
+    public void saveGame() {
+    }
+
+    @Test
+    public void loadGame() {
+    }
+}
